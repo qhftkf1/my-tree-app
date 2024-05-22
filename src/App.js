5bdc24160a6708b56d5395068a18148b9c8219b5
BAITHINGS-SONG <song@baithings.co.kr>
2024-05-23 01:27:01 +0900

    # First commit # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #    modified:   package-lock.json #    modified:   package.json #    modified:   src/App.css #    modified:   src/App.js # # Untracked files: #    src/assets/ #    src/component/ #

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './component/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>        
   );
 }
 
 export default App;
+// Router를 사용하는 이유
+/* 
+ React는 아래와 같은 이슈가 있다. 
+ 1) React는 SPA이므로 새로고침을 하면 현재 페이지의 상태를 잃는다. 
+ 2) 브라우저의 기본 기능이 페이지 이동 기능이 상실된다. 
+ 이를 해결하기 위해 React Router를 사용한다. 
+*/
